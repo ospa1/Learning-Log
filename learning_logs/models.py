697f3904fd1fae37577ec1af5d0ee9098a2e08aa
ospa1 <oscarkil09@yahoo.com>
2020-10-10 21:31:41 -0500

    added a template url

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,8 +5,31 @@ from django.db import models
 class Topic(models.Model):
 
     """ A topic a user is learning about """
+
+    # Create text and date field
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
+
+
+class Entry(models.Model):
+
+    """ Specifically learned about a topic"""
+
+    # on_delete=models.CASCADE arg will  delete all entries when a topic is deleted
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    # Create text and date field
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # Tells Django to use Entries when it needs to refer to more than one entry
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        # will only show the first 50 chars of an entry
+        # ellipsis clarifies that we are not always displaying the entire entry
+        return f"{self.text[:50]}..."
