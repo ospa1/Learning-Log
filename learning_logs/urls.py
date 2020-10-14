195ffc01f868d849cb19d29c010276ff41982c81
ospa1 <oscarkil09@yahoo.com>
2020-10-14 14:14:04 -0500

    added individual topics and entries pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
+    # topics page
+    path('topics/', views.topics, name='topics'),
+    # single topics page
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from learning_logs.models import Topic
+
+
 def index(request):
     """ Hompage """
     return render(request, 'learning_logs/index.html')
+
+
+# shows all topics
+def topics(request):
+    all_topics = Topic.objects.order_by('date_added')
+    context = {'topics': all_topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+# shows entries for a topic
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)  # todo rename variable so it doesnt collide with function name
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
